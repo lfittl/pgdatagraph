0af194e582195d850c158cd2f7b80359556a61ad
Lukas Fittl <lukas@fittl.com>
2014-06-30 13:53:18 +0200

    Update moment and d3, reference (most) required libs in pgdatagraph.js directly.

--- app/assets/javascripts/pgdatagraph.js
+++ app/assets/javascripts/pgdatagraph.js
@@ -1,3 +1,9 @@
+//#= require underscore
+//= require d3.v3.min
+//= require rickshaw
+//= require moment.min
+//#= require jquery-ui
+
 //= require pgdatagraph/pg
 //= require pgdatagraph/palette
 //= require pgdatagraph/brush
